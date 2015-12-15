04e0bb0d9ac8492933258ea89f14144f5ebca517
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-12-15 17:30:58 +0100

    fix edit tools checks and image link

--- geonode/contrib/worldmap/context_processors.py
+++ geonode/contrib/worldmap/context_processors.py
@@ -1,10 +1,12 @@
 import settings
 
+from geonode.geoserver.helpers import ogc_server_settings
 
 def worldmap_urls(request):
     """Global values to pass to templates"""
     defaults = dict(
-        GEONODE_CLIENT_LOCATION=settings.GEONODE_CLIENT_LOCATION
+        GEONODE_CLIENT_LOCATION=settings.GEONODE_CLIENT_LOCATION,
+        DB_DATASTORE=True if 'postgis' in ogc_server_settings.datastore_db['ENGINE'] else False
     )
 
     return defaults
