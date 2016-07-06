142b1ce48693dad554f36c587aebb728dd5907bd
Jeffrey Johnson <ortelius@gmail.com>
2016-07-06 15:08:42 -0700

    create a link ...almost

--- geonode/contrib/mp/signals.py
+++ geonode/contrib/mp/signals.py
@@ -1,5 +1,7 @@
 from geonode.layers.models import Layer
+from geonode.base.models import Link
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
 import sys
 
 def tileset_post_save(instance, sender, **kwargs):
@@ -8,5 +10,18 @@ def tileset_post_save(instance, sender, **kwargs):
     except ObjectDoesNotExist:
         layer = Layer(name=instance.name)
         layer.save()
+        #tile_url = reverse('tileset_mapproxy', kwargs={'pk':instance.id, 'path_info':'abc'})
+        tile_url = "abc" 
+
+        Link.objects.get_or_create(
+            resource=layer.resourcebase_ptr,
+            url=tile_url,
+            defaults=dict(
+                extension='tiles',
+                name="Tiles",
+                mime='image/png',
+                link_type='image'
+            )
+        )
     except:
         print sys.exc_info()[0]
