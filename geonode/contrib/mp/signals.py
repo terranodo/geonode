4b36087bf813b92e0cee43eed66276e100257c97
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-07-08 12:20:44 +0200

    make the layer appear on the map, adjust the signal a bit

--- geonode/contrib/mp/signals.py
+++ geonode/contrib/mp/signals.py
@@ -6,13 +6,15 @@ import sys
 
 def tileset_post_save(instance, sender, **kwargs):
     try:
-        existing_layer = Layer.objects.get(name=instance.name) 
-    except ObjectDoesNotExist:
-        layer = Layer(name=instance.name)
-        layer.save()
-        #tile_url = reverse('tileset_mapproxy', kwargs={'pk':instance.id, 'path_info':'abc'})
-        tile_url = "abc" 
+        existing_layer = Layer.objects.filter(name=instance.name).exists()
 
+        if not existing_layer:
+            layer = Layer(name=instance.name)
+            layer.save()
+        else:
+            layer = Layer.objects.get(name=instance.name)     
+               
+        tile_url = "/djmp/1/map/tiles/%s/EPSG3857/{z}/{x}/{y}.png" % instance.name
         Link.objects.get_or_create(
             resource=layer.resourcebase_ptr,
             url=tile_url,
