14fefa936c169f58bcf282999b5c63046db20976
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-07-26 14:30:20 +0200

    add standard settings and geocode authorisation for djmp tilesets

--- geonode/contrib/mp/settings.py
+++ geonode/contrib/mp/settings.py
@@ -1,8 +1,9 @@
-from django.conf import settings
 
 # make geonode mapping clients to use tiles directly from the disk
 # REQURES WEB SERVER CONFIGURATION TO SERVE FILE_CACHE_DIRECTORY
-USE_DISK_CACHE = getattr(settings, 'USE_DISK_CACHE', False)
+USE_DISK_CACHE = False
 
 # create mapproxy cache for all geonode layers
-USE_DJMP_FOR_GEONODE_LAYERS = getattr(settings, 'USE_DJMP_FOR_GEONODE_LAYERS', False)
+USE_DJMP_FOR_GEONODE_LAYERS = True
+
+DJMP_AUTHORIZATION_CLASS = 'geonode.contrib.mp.authorization.GeoNodeDJMPAuthorization'
